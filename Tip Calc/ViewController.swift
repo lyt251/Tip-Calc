f3adb5c3192f665c74ccc5b8e92caa5004847d2e
lyt251 <69009242+lyt251@users.noreply.github.com>
2020-08-18 22:43:29 -0700

    Initial Commit

---
+++ Tip Calc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calc
+//
+//  Created by Tina Ly on 8/18/20.
+//  Copyright © 2020 Tina Ly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
